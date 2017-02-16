150ee660334482299d82e65b6cb441281dfa968d
Sharlene <sharlenemendez@gmail.com>
2017-02-16 11:55:11 -0500

    E06 Finishdd ICE

--- E06_Transformations/AppClass.cpp
+++ E06_Transformations/AppClass.cpp
@@ -19,16 +19,36 @@ void AppClass::InitVariables(void)
 
 	//Initializing the primitives
 	m_pSphere->GenerateSphere(0.5f, 5, REWHITE);
+	m_pCone->GenerateCone(0.5f, 0.5f, 10, REGREEN);
+	m_pCylinder->GenerateCylinder(0.5f, 0.5f, 10, REBLUE);
+	m_pTube->GenerateTube(0.5f, 0.25f, 0.5f, 10, REBROWN);
+	m_pTorus->GenerateTorus(0.5f, 0.25f, 10, 10, REYELLOW);
+	m_pCube->GenerateCube(1.0f, vector3(1, 0, 0));
+
 }
 
 void AppClass::Update(void)
 {
 	//This matrices will just place the objects int the right spots
 	m_m4Sphere = glm::translate(IDENTITY_M4, vector3(2.5f, 2.5f, 0.0f));
+	
+	// My code
+	m_m4Cone = glm::translate(IDENTITY_M4, vector3(0.0f, 2.0f, 0.0f));
+	m_m4Cylinder = glm::translate(IDENTITY_M4, vector3(-2.0f, 0.0f,0.0f));
+	m_m4Tube = glm::translate(IDENTITY_M4, vector3(0.0f, -2.0f, 0.0f));
+	m_m4Torus = glm::translate(IDENTITY_M4, vector3(0.0f, 0.0f, 0.0f));
+	m_m4Cube = glm::translate(IDENTITY_M4, vector3(2.0f, 0.0f, 0.0f));
 
 	//This matrices will scale them to the right size
 	m_m4Sphere = glm::scale(m_m4Sphere, vector3(2.0f, 2.0f, 2.0f));
-
+	
+	// My code
+	m_m4Cone = glm::scale(m_m4Cone, vector3(2.0f, 2.0f, 2.0f));
+	m_m4Cylinder = glm::scale(m_m4Cylinder, vector3(2.0f, 2.0f, 2.0f));
+	m_m4Tube = glm::scale(m_m4Tube, vector3(2.0f, 2.0f, 2.0f));
+	m_m4Torus = glm::scale(m_m4Torus, vector3(2.0f, 2.0f, 2.0f));
+	m_m4Cube = glm::scale(m_m4Cube, vector3(1.0f, 1.0f, 1.0f));
+	
 	//Indicate the FPS
 	int nFPS = m_pSystem->GetFPS();
 	//Print info on the screen
@@ -49,7 +69,14 @@ void AppClass::Display(void)
 
 	//Renders the meshes using the specified position given by the matrix and in the specified color
 	m_pSphere->Render(m4Projection, m4View, m_m4Sphere);
-	
+
+	// My code
+	m_pCone->Render(m4Projection, m4View, m_m4Cone);
+	m_pCylinder->Render(m4Projection, m4View, m_m4Cylinder);
+	m_pTube->Render(m4Projection, m4View, m_m4Tube);
+	m_pTorus->Render(m4Projection, m4View, m_m4Torus);
+	m_pCube->Render(m4Projection, m4View, m_m4Cube);
+
 	//Render the grid based on the camera's mode:
 	m_pMeshMngr->AddGridToRenderListBasedOnCamera(m_pCameraMngr->GetCameraMode());
 	m_pMeshMngr->Render(); //renders the render list
